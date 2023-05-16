0ad77875d6c930fadb987e085471147917de0863
Christian Bosse <christianbosse1998@gmail.com>
2023-05-16 07:28:51 -0300

    added saving and displaying

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,9 +15,64 @@ const RenderCards = ({ data, title }) => {
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
-    const [allPost, setAllPost] = useState(null);
+    const [allPosts, setAllPosts] = useState();
 
     const [searchText, setSearchText] = useState("");
+
+    const [searchResults, setSearchResults] = useState(null);
+    const [searchTimeout, setSearchTimeout] = useState(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+
+            try {
+                const response = await fetch(
+                    "http://localhost:8080/api/v1/post",
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+
+                if (response.ok) {
+                    const result = await response.json();
+
+                    setAllPosts(result.data.reverse());
+                }
+            } catch (error) {
+                alert(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
+
+        setSearchText(e.target.value);
+
+        setSearchTimeout(
+            setTimeout(() => {
+                const searchResults = allPosts.filter(
+                    (item) =>
+                        item.name
+                            .toLowerCase()
+                            .includes(searchText.toLowerCase()) ||
+                        item.prompt
+                            .toLowerCase()
+                            .includes(searchText.toLowerCase())
+                );
+
+                setSearchResults(searchResults);
+            }, 500)
+        );
+    };
 
     return (
         <section className="max-w-7xl mx-auto">
@@ -31,7 +86,14 @@ const Home = () => {
                 </p>
             </div>
             <div className="mt-16">
-                <FormField />
+                <FormField
+                    labelName="Search posts"
+                    type="text"
+                    name="text"
+                    placeholder="Search posts"
+                    value={searchText}
+                    handleChange={handleSearchChange}
+                />
             </div>
 
             <div className="mt-10">
@@ -52,11 +114,14 @@ const Home = () => {
                         <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                             {searchText ? (
                                 <RenderCards
-                                    data={[]}
+                                    data={searchResults}
                                     title="No search results found"
                                 />
                             ) : (
-                                <RenderCards data={[]} title="No posts found" />
+                                <RenderCards
+                                    data={allPosts}
+                                    title="No posts found"
+                                />
                             )}
                         </div>
                     </>
